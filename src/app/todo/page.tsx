5d8958397bd9f51451745490b09a9b6ccab3e261
Reshwin Ravindran <reshwin.ravindran@gmail.com>
2025-05-22 10:59:34 +0530

    todo page

--- src/app/todo/page.tsx
+++ src/app/todo/page.tsx
@@ -1,54 +1,88 @@
 'use client'
-import React, { useState } from 'react'
+import { Button, Stack } from '@mui/material'
+import React, { useEffect, useState } from 'react'
 
 //name, desc, date, status, id
 interface TaskType {
-	userName: string
-	userId: string
-	userStatus: string
+	name: string
+	taskId: string
+	status: string
 }
 const TodoApp = () => {
 	const [todos, setTodos] = useState<TaskType[]>([])
-	//   const [uName, setUname] = useState(String)
-	const [task, setTask] = useState<TaskType>({})
-	const handleChange = (event: React.FocusEventHandler<HTMLInputElement>) => {
-		const { name, value } = event?.target
-		console.log({ name, value })
-		setTask({ ...task, [name]: value })
-		// const sampleTask = {...task}
-		// sampleTask[name] = value
-		// setTask(sampleTask)
+	const [task, setTask] = useState<TaskType>({
+		name: '',
+		taskId: '',
+		status: '',
+	})
+	const [disabledAdd, setDisableAdd] = useState(true)
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const { name, value } = event.target
+		// console.log({ name, value })
+		setTask(prev => ({ ...prev, [name]: value }) as TaskType)
 	}
+	// console.log({task,todos})
 	const handleSubmit = () => {
+		console.log('---', task)
+		setDisableAdd(false)
 		setTodos(prev => [...prev, task])
+		setTask({ name: '', taskId: '', status: '' })
 	}
+	useEffect(() => {
+		if (task.name && task.status && task.taskId) {
+			setDisableAdd(false)
+		} else {
+			setDisableAdd(true)
+		}
+	}, [task])
 
-	console.log(task, todos)
+	console.log({ task, todos })
 	return (
 		<>
-			<div>TodoApp</div>
-			<input type='text' name='userName' id='name' onBlur={handleChange} />
-			{/* <input
-        type="text"
-        name="description"
-        id="description"
-        onBlur={handleChange}
-      /> */}
-			<input type='text' name='userStatus' id='status' onBlur={handleChange} />
-			<input type='text' name='userId' id='id' onBlur={handleChange} />
-			<button onClick={handleSubmit}>Add</button>
+			<h1 style={{ marginBottom: '16px' }}>TodoApp</h1>
+			<input
+				type='text'
+				name='name'
+				id='name'
+				value={task.name}
+				onChange={handleChange}
+				placeholder='Task Name'
+			/>
+			<input
+				type='text'
+				name='status'
+				id='status'
+				value={task.status}
+				onChange={handleChange}
+				placeholder='Task Status'
+			/>
+			<input
+				type='text'
+				name='taskId'
+				id='id'
+				value={task.taskId}
+				onChange={handleChange}
+				placeholder='Task Id'
+			/>
+			<Button variant='contained' disabled={disabledAdd} onClick={handleSubmit}>
+				Add
+			</Button>
 
-			{todos.map(task => {
-				return (
-					<div key={task.userId}>
-						<p>{task.userName}</p>
-						<p>{task.userStatus}</p>
-						<p>{task.userId}</p>
-						{/* <p>{task.name}</p>
-            <p>{task.name}</p> */}
-					</div>
-				)
-			})}
+			<Stack gap='16px' sx={{ mt: 3 }}>
+				{todos.map((task, index) => {
+					return (
+						<Stack direction='row' gap='16px' key={task.taskId}>
+							<p>{index + 1}.</p>
+							<p>{task.name}</p>
+							<hr />
+							<p>{task.status}</p>
+							<hr />
+							<p>{task.taskId}</p>
+							<hr />
+						</Stack>
+					)
+				})}
+			</Stack>
 		</>
 	)
 }
